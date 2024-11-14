d6917d004decddffd21edf0fdd98ecdc56faa8d6
MantYcore. <akshaymadathil3@gmail.com>
2024-11-14 23:44:55 +0530

    4  commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React, { Children } from 'react'
+
+const Card = ({children, bg = 'bg-blue-200' }) => {
+  return (
+    <div className={`${bg} p-6 rounded-lg shadow-md`}>
+{children}
+
+    </div>
+  )
+}
+
+export default Card
---
+++ src/components/JobListings.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Jobs from '../jobs.json'
+import JobListing from './JobListing'
+const JobListings = () => {
+
+
+  return (
+    <section class="bg-blue-50 px-4 py-10">
+    <div class="container-xl lg:container m-auto">
+      <h2 class="text-3xl font-bold text-indigo-500 mb-6 text-center">
+        Browse Jobs
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+{Jobs.map((job)=>(
+  
+<JobListing key={job.id} job={ job}/>
+))}
+
+        {/* <!-- Job Listing 1 --> */}
+      
+        {/* <!-- Job Listing 2 --> */}
+        
+        {/* <!-- Job Listing 3 --> */}
+       
+      </div>
+    </div>
+  </section>
+  )
+}
+
+export default JobListings
