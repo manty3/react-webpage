02690e5bcdd15017d2fd54bef0a64a8d34236732
MantYcore. <akshaymadathil3@gmail.com>
2024-11-25 23:01:08 +0530

    25/11/24 success 2d  commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,37 @@ import HomePage from './pages/HomePage'
 import JobsPage from './pages/jobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage from './pages/JobPage'
+import AddJob from './pages/AddJob'
 
 
-
-const router = createBrowserRouter(
-  createRoutesFromElements (
-    <Route path='/' element = {<MainLayout/>}>
-      <Route index  element = {<HomePage/>} />
-      <Route path='/jobs' element = {<JobsPage/>}/>
-      <Route path='/jobs/:id' element = {<JobPage/>}  />
-      <Route path='*' element = {<NotFoundPage/>}/>
-      
-      </Route>
-   
-  )
-)
 
 const App = () => {
+
+  const addJob = async (newJob) => {
+const res = await fetch ('/api/jobs', {
+method:'POST',
+headers:{
+
+  'content-type': 'application/json',
+},
+body: JSON.stringify(newJob),
+
+})
+  return;
+  }
+  const router = createBrowserRouter(
+    createRoutesFromElements (
+      <Route path='/' element = {<MainLayout/>}>
+        <Route index  element = {<HomePage/>} />
+        <Route path='/jobs' element = {<JobsPage/>}/>
+        <Route path='/add-job' element = {<AddJob/>} addJobSubmit={addJob}/>
+        <Route path='/jobs/:id' element = {<JobPage/>}  />
+        <Route path='*' element = {<NotFoundPage/>}/>
+        
+        </Route>
+     
+    )
+  )
   return <RouterProvider router={router}/>
 }
 
